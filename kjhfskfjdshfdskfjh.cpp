7a802231cc892aef404e87e2fd6513cb5c8ad6c6
akiyamakai <grusha.cs@gmail.com>
2024-03-15 09:15:25 +0200

    Добавьте файлы проекта.

---
+++ kjhfskfjdshfdskfjh.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+               
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    const int SIZE = 5; 
+    int arr[SIZE] = { 5, 2, 8, 1, 9 }; 
+
+    
+    std::cout << "Array to be sorted: ";
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    
+    bubbleSort(arr, SIZE);
+
+    
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
